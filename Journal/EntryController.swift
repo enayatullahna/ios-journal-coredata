641d5d4a4182be459f1586e8727778d21fad588d
Enayatullah Naseri <ena.nas@icloud.com>
2019-12-07 09:23:12 -0800

    completed Entry+Convenience but getting an error use of undeclared type

---
+++ Journal/EntryController.swift
@@ -0,0 +1,14 @@
+//
+//  EntryController.swift
+//  Journal
+//
+//  Created by Enayatullah Naseri on 12/7/19.
+//  Copyright © 2019 Enayatullah Naseri. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class EntryController {
+    
+}
