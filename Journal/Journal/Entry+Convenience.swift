4f4cbb6585556b4825650bc50189779035bfb12d
Enayatullah Naseri <ena.nas@icloud.com>
2019-12-10 18:48:29 -0800

    Added new model version and updated entry+convenience file and added entry rep

--- Journal/Journal/Entry+Convenience.swift
+++ Journal/Journal/Entry+Convenience.swift
@@ -14,12 +14,19 @@ enum EntryMood: String, CaseIterable {
     case 😡
     case 😐
     case 🤪
-    
 }
 
 extension Entry {
     
-    convenience init(title: String, bodyText: String, timestamp: Date = Date(), identifier: String = UUID().uuidString, mood: EntryMood = .🤪, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+    var entryRepresentation: EntryRepresentation? {
+        guard let title = self.title,
+            let mood = self.mood,
+            let timestamp = self.timestamp,
+            let identifier = self.identifier else {return nil}
+        
+        return EntryRepresentation(title: title, bodyText: bodyText, mood: mood, timestamp: timestamp, identifier: identifier)
+    }
+    convenience init(title: String, bodyText: String?, timestamp: Date = Date(), identifier: String = UUID().uuidString, mood: EntryMood = .🤪, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
         self.init(context: context)
         self.title = title
         self.bodyText = bodyText
@@ -28,4 +35,16 @@ extension Entry {
         self.mood = mood.rawValue
     }
     
+    // Initialize an entry object from EntryRepresentation
+    convenience init?(entryRepresentation: EntryRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        guard let mood = EntryMood(rawValue: entryRepresentation.mood!),
+            let identifier = UUID(uuidString: entryRepresentation.identifier) else { return nil }
+        
+        self.init(title: entryRepresentation.title,
+                  bodyText: entryRepresentation.bodyText,
+                  timestamp: entryRepresentation.timestamp,
+                  identifier: identifier.uuidString,
+                  mood: mood,
+                  context: context)
+    }
 }
